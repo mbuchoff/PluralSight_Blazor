1b157c66df966860673b5c8958925ae09a9b37d6
safemarkMBuchoff <mbuchoff@safemark.com>
2020-01-09 09:45:59 -0500

    Added edit pages

---
+++ BethanysPieShopServer/Pages/EmployeeEditBase.cs
@@ -0,0 +1,27 @@
+﻿using BethanysPieShopHRM.Shared;
+using BethanysPieShopServer.Services;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShopServer.Pages
+{
+    public class EmployeeEditBase : ComponentBase
+    {
+        [Inject]
+        public IEmployeeDataService EmployeeDataService { get; set; }
+
+        [Parameter]
+        public string EmployeeId { get; set; }
+
+        public Employee Employee { get; set; } = new Employee();
+
+        protected override async Task OnInitializedAsync()
+        {
+            Employee = await EmployeeDataService.GetEmployeeDetails(int.Parse(EmployeeId));
+        }
+	}
+}
