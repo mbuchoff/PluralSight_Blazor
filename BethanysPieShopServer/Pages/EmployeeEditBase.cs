58228aa29b20a5594fe1d013e82473ac55adc607
safemarkMBuchoff <mbuchoff@safemark.com>
2020-01-11 11:35:19 -0500

    Saving the Data

--- BethanysPieShopServer/Pages/EmployeeEditBase.cs
+++ BethanysPieShopServer/Pages/EmployeeEditBase.cs
@@ -20,6 +20,9 @@ namespace BethanysPieShopServer.Pages
         [Inject]
         public IJobCategoryDataService JobCategoryDataService { get; set; }
 
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
         [Parameter]
         public string EmployeeId { get; set; }
 
@@ -31,15 +34,75 @@ namespace BethanysPieShopServer.Pages
         protected string CountryId = string.Empty;
         protected string JobCategoryId = string.Empty;
 
+        protected string Message = string.Empty;
+        protected string StatusClass = string.Empty;
+        protected bool Saved;
+
         protected override async Task OnInitializedAsync()
         {
-            Employee = await EmployeeDataService.GetEmployeeDetails(int.Parse(EmployeeId));
+            Saved = false;
             Countries = (await CountryDataService.GetAllCountries()).ToList();
             JobCategories = (await JobCategoryDataService.GetAllJobCategories()).ToList();
+
+            int.TryParse(EmployeeId, out var employeeId);
+
+            if (employeeId == 0)
+            {
+                Employee = new Employee { CountryId = 1, JobCategoryId = 1, BirthDate = DateTime.Now };
+            }
+            else
+            {
+                Employee = await EmployeeDataService.GetEmployeeDetails(int.Parse(EmployeeId));
+            }
+
             CountryId = Employee.CountryId.ToString();
-            JobCategoryId = 
-                
-                Employee.JobCategoryId.ToString();
+            JobCategoryId = Employee.JobCategoryId.ToString();
         }
-	}
+
+        protected async Task HandleValidSubmit()
+        {
+            Saved = false;
+            Employee.CountryId = int.Parse(CountryId);
+            Employee.JobCategoryId = int.Parse(JobCategoryId);
+
+            if (Employee.EmployeeId == 0) //new
+            {
+                var addedEmployee = await EmployeeDataService.AddEmployee(Employee);
+                if (addedEmployee != null)
+                {
+                    StatusClass = "alert-success";
+                    Message = "New employee added successfully.";
+                    Saved = true;
+                }
+                else
+                {
+                    StatusClass = "alert-danger";
+                    Message = "Something went wrong adding the new employee. Please try again.";
+                    Saved = false;
+                }
+            }
+            else
+            {
+                await EmployeeDataService.UpdateEmployee(Employee);
+                StatusClass = "alert-success";
+                Message = "Employee updated successfully.";
+                Saved = true;
+            }
+        }
+
+        protected async Task DeleteEmployee()
+        {
+            await EmployeeDataService.DeleteEmployee(Employee.EmployeeId);
+
+            StatusClass = "alert-success";
+            Message = "Deleted successfully";
+
+            Saved = true;
+        }
+
+        protected void NavigateToOverview()
+        {
+            NavigationManager.NavigateTo("/employeeoverview");
+        }
+    }
 }
--- BethanysPieShopServer/Services/EmployeeDataService.cs
+++ BethanysPieShopServer/Services/EmployeeDataService.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
 
@@ -17,14 +18,24 @@ namespace BethanysPieShopServer.Services
             _httpClient = httpClient;
         }
 
-        public Task<Employee> AddEmployee(Employee employee)
+        public async Task<Employee> AddEmployee(Employee employee)
         {
-            throw new NotImplementedException();
+            var employeeJson =
+                new StringContent(JsonSerializer.Serialize(employee), Encoding.UTF8, "application/json");
+
+            var response = await _httpClient.PostAsync("api/employee", employeeJson);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return await JsonSerializer.DeserializeAsync<Employee>(await response.Content.ReadAsStreamAsync());
+            }
+
+            return null;
         }
 
-        public Task DeleteEmployee(int employeeId)
+        public async Task DeleteEmployee(int employeeId)
         {
-            throw new NotImplementedException();
+            await _httpClient.DeleteAsync($"api/employee/{employeeId}");
         }
 
         public async Task<IEnumerable<Employee>> GetAllEmployees()
@@ -41,9 +52,12 @@ namespace BethanysPieShopServer.Services
                 new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
         }
 
-        public Task UpdateEmployee(Employee employee)
+        public async Task UpdateEmployee(Employee employee)
         {
-            throw new NotImplementedException();
+            var employeeJson =
+                new StringContent(JsonSerializer.Serialize(employee), Encoding.UTF8, "application/json");
+
+            await _httpClient.PutAsync("api/employee", employeeJson);
         }
     }
 }
